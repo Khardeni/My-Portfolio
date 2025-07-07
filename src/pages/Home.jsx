86b8283cc30c138572d9952833d14bd9faa48829
Khardeni <82120858+Khardeni@users.noreply.github.com>
2025-07-07 15:10:33 +0100

    Projects Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,35 @@
+import { Navbar } from '../components/Navbar';
+import { HeroSection } from '../components/HeroSection';
+import { AboutSection } from '../components/AboutSection';  
+import { SkillsSection } from '../components/SkillsSection';
+import { ProjectSection } from '../components/ProjectSection';
+import { ContactSection } from '../components/ContactSection';
+import { Footer } from '../components/Footer';
+import { ThemeToggle } from '../components/ThemeToggle';
+import { StarBackground } from '../components/StarBackground';
+
+
 export const Home = () => {
-    return (
-        <div> Home Page
-        </div>
-    );
-}
+  return (
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Background Effects */}
+      <StarBackground />
+
+      {/* Navbar */}
+      <Navbar />
+      {/* Main Content */}
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ProjectSection />
+        <ContactSection />
+      </main>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+};
